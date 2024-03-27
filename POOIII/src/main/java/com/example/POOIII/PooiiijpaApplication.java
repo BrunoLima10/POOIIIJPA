0c03313ff9ad483d439e6034f62a23dcdef1116e
Bruno Lima <nscbrunolima@gmail.com>
2024-03-27 11:01:20 -0300

    POOIIIjpa

---
+++ POOIII/src/main/java/com/example/POOIII/PooiiijpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.POOIII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooiiijpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooiiijpaApplication.class, args);
+	}
+
+}
